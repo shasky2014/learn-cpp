b39230e4b54bcf40272c3b6b483fc4e023373db5
neon <249398363@qq.com>
2024-12-07 20:20:47 +0800

    引用

--- cpp_primer_plus_6/chapter08/Listing_8_1.cpp
+++ cpp_primer_plus_6/chapter08/Listing_8_1.cpp
@@ -10,5 +10,27 @@ int main(int argc, char const *argv[])
     cout << "a = " << a << " ,b=" << b << " ,c=" << c << endl;
     cout << "sq(c++) = " << sq(c++) << endl;
     cout << "c = " << c << endl;
+    /*listing 8.2 引用，不同于指针，引用只是别名，只能在引用时定义，无法通过赋值修改引用对象
+     */
+    int rats = 20093;
+    int &rodents = rats;
+    cout << "rats = " << rats;
+    cout << ", rodents = " << rodents << endl;
+    rodents++;
+    cout << "do rodents++ " << endl;
+    cout << "rats = " << rats;
+    cout << ", rodents = " << rodents << endl;
+
+    cout << "rats address = " << &rats << endl;
+    cout << "rodents address = " << &rodents << endl;
+    int bats = 50093;
+    rodents = bats;
+    cout << "do rodents = bats " << endl;
+    cout << "rats = " << rats;
+    cout << ", rodents = " << rodents << endl;
+    cout << "bats = " << bats << endl;
+    cout << "rats address = " << &rats << endl;
+    cout << "bats address = " << &bats << endl;
+    cout << "rodents address = " << &rodents << endl;
     return 0;
 }
