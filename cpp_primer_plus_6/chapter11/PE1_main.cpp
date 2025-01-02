98a80bfe27712dfed05b971441bd3ada280e31e3
neon <249398363@qq.com>
2025-01-02 18:20:46 +0800

    PE3

--- cpp_primer_plus_6/chapter11/PE1_main.cpp
+++ cpp_primer_plus_6/chapter11/PE1_main.cpp
@@ -25,35 +25,49 @@ int main(int argc, char const *argv[])
     cout << "Enter target distance (q to quit):";
     while (cin >> target)
     {
+        int steps_count, n, max_step, min_step, sum_step;
         loop_times += 1;
-        int steps_count = 0;
         ofstream fout("PE1_out" + to_string(loop_times) + ".txt");
         cout << "Enter step length:";
         if (!(cin >> dstep))
             break;
+
+        cout << "Enter loop times:";
+        if (!(cin >> n))
+            break;
         fout << "Target distance: " << target << " step size:" << dstep << endl;
-        while (result.d() < target)
+        max_step = min_step = sum_step = 0;
+        for (int i = 0; i < n; i++)
         {
-            direction = rand() % 360;
-            step.reset(dstep, direction, Vector::POL);
-            step.RECT_mode();
-            result = result + step;
-            fout << steps_count << ":walk " << step << " to " << result << endl;
-            steps_count += 1;
+            steps_count = 0;
+            while (result.d() < target)
+            {
+                direction = rand() % 360;
+                step.reset(dstep, direction, Vector::POL);
+                step.RECT_mode();
+                result = result + step;
+                fout << "loop " << i << " ,step " << steps_count << " :walk " << step << " to " << result << endl;
+                steps_count += 1;
+            }
+            max_step = (steps_count < max_step) ? max_step : steps_count;
+            min_step = (steps_count > min_step) ? min_step : steps_count;
+            sum_step += steps_count;
+
+            cout << steps_count << " steps to the target" << endl;
+            cout << result << endl;
+            cout << "Average outward distance per step is " << result.d() / steps_count << endl;
+
+            fout << "cost " << steps_count << " steps to the target" << endl
+                 << result;
+            result.POL_mode();
+            fout << " or " << result << endl
+                 << "Average outward distance per step is " << result.d() / steps_count << endl;
+            result.reset();
+            step = 0;
         }
-        cout << steps_count << " steps to the target" << endl;
-        cout << result << endl;
-        cout << "Average outward distance per step is " << result.d() / steps_count << endl;
-
-        fout << "cost " << steps_count << " steps to the target" << endl
-             << result;
-        result.POL_mode();
-        fout << " or " << result << endl
-             << "Average outward distance per step is " << result.d() / steps_count << endl;
+        cout << "max_step= " << max_step << " min_step= " << min_step << " avg_step= " << sum_step / n << endl;
+        fout << "After " << n << " loops, max_step= " << max_step << " min_step= " << min_step << " avg_step= " << sum_step / n << endl;
         fout.close();
-
-        step = 0;
-        result.reset();
         cout << "Enter target distance (q to quit):";
     }
     cout << "Bye!" << endl;
